2b470ae84d89288173564edd02ea9b2d5a342801
Pavel Prokopenya <pavel12minsk@mail.ru>
2021-07-05 10:27:13 +0300

    Minor Program.cs changes

--- TestApp2/Program.cs
+++ TestApp2/Program.cs
@@ -7,6 +7,7 @@ namespace TestApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm a GIT-Shared! YOHO!");
         }
     }
 }
